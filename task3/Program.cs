d874dfc22b97b42f593bb32c9ccd92f05db0a7c9
Anastasia-2-0 <nosova0702@mail.ru>
2022-10-05 12:18:55 +0300

    решеине 3 зачачи

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.WriteLine("Введите цифру от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+    if( a == 6 || a == 7)
+    {
+        Console.WriteLine("yes");
+    }
+    else if( a > 7)
+    {
+        Console.WriteLine("error");
+    }
+    else if(a < 6)
+    {
+        Console.WriteLine("no");
+    }
