6afe203b105c94a8d28ad2456da317550c5c3e15
Anastasia-2-0 <nosova0702@mail.ru>
2022-10-05 13:20:23 +0300

    решение 1 зачачи

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int num2 = 0;
+if(num > 99 || num < 1000)
+{
+    num2 = num / 10;
+    num2 = num2 % 10;
+}
+else
+{
+    Console.WriteLine("error");
+}
+Console.WriteLine($"Вторая цифра числа{num} равна {num2}");
