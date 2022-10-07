57c4ceea81d93916bc449f2da12cd6d02b15e99e
Anastasia-2-0 <nosova0702@mail.ru>
2022-10-07 13:29:43 +0300

    первое решеине 2 задачи

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+ void FillArray(int[] collection){
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+
+
+int[] array =new int[new Random().Next(1,20)];
+int length = array.Length;
+
+if(length < 3){
+    Console.WriteLine("Третьей цифры нет");
+}
+else{
+    FillArray(array);
+    PrintArray(array);
+    Console.WriteLine($" Третий элемент данного массива " +  array[2]);
+}
+
